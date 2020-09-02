35b3ff6d6d65f026b28198c7fd5f855dc248abd3
fletcher17 <ororo17@gmail.com>
2020-09-02 08:30:46 +0100

    map, leaftlet installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/c
 import InfoBox from './Components/InfoBox/Infobox';
 import Table from './Components/Table/Table';
 import {sortData} from './Components/Utils';
-import Map from './Components/Map';
+import LineGraph from './Components/LineGraph/LineGraph';
+import Map from './Components/Map/Map';
+import 'leaflet/dist/leaflet.css';
 import './App.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({lat: 34.80746, lng: -40.4796});
+  const [mapZoom, setMapZoom] = useState(3);
 
 
 useEffect(() => {
@@ -69,18 +73,18 @@ useEffect (() => {
         </FormControl>
        </div>
         <div className='app__stats'>
-          <InfoBox title='coronavirus cases' cases={countryInfo.todayCases} total={2000}/>
-          <InfoBox title='Coronavirus recoveries' cases={countryInfo.todayRecovered} total={3000}/>
-          <InfoBox title='coronavirus deaths' cases={countryInfo.cases} total={4000}/>
+          <InfoBox title='coronavirus cases' cases={countryInfo.todayCases} total={countryInfo.cases}/>
+          <InfoBox title='Coronavirus recoveries' cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+          <InfoBox title='coronavirus deaths' cases={countryInfo.cases} total={countryInfo.deaths}/>
         </div>
-          <Map/>
+          <Map center={mapCenter} zoom={mapZoom}/>
       </div>
       <Card className='app__right'>
         <CardContent>
           <h3>Live Cases by Country</h3>
               <Table countries={tableData}/>
           <h3>Worldwide new cases</h3>
-        {/* Graph */}
+          <LineGraph/>
         </CardContent>
       </Card>
     </div>
--- src/Components/InfoBox/Infobox.jsx
+++ src/Components/InfoBox/Infobox.jsx
@@ -6,11 +6,8 @@ const InfoBox = ({title, cases, total}) => {
 	return (
 		<Card className='infoBox'>
 		   <CardContent>
-		{/* Titlte i.e Coronavirus cases */}
 		<Typography className='infoBox__title' color='textSecondary'>{title}</Typography>
-		{/* +120 Number of cases */}
 		<h2>{cases}</h2>
-		{/* 1.2M total cases */}
 		<Typography className='infoBox__total' color='textSecondary'>{total}</Typography>
 		   </CardContent>
 		</Card>
